163fb80c6188bd140dd2cd262ff7bf1875e2d1c6
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-11 22:56:31 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { FC } from 'react';
-import {StateProvider} from './store/index';
+import { StateProvider } from './store/index';
 import Dashboard from './views/Dashboard';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router/Router'
+import Navbar from './components/navigation/Navbar';
 
 const App: FC = () => {
- 
+
   return (
     <StateProvider>
-    <div className="App">
-    <div className="App">
-        <header className="App-header">
-          <h1>Counter Example</h1>
-        </header>
-        <Dashboard />
-      </div>
-    </div>
+      <BrowserRouter >
+        <Navbar />
+        <div className="App">
+          <div className="App">
+            <header className="App-header">
+              <h1>Counter Example</h1>
+            </header>
+            <Dashboard />
+          </div>
+        </div>
+        <Router />
+      </BrowserRouter>
     </StateProvider>
   );
 }
